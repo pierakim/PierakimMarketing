410efbb576cbb68a7007614b7125b31ea06bb37e
pierakim.lambda <pierakim.lambda@gmail.com>
2019-06-09 17:05:31 +1000

    update links + lambda logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import Container from 'react-bootstrap/Container';
 import Layout from "../components/layout"
 import SocialImages from "../components/socialImages"
-import LambdaImage from "../components/lambdaImage"
+import StyledLambdaImage from "../components/lambdaImage"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -13,7 +13,7 @@ const IndexPage = () => (
       <div>
       {/* <Container style={{ maxWidth: `100px`, marginBottom: `1.45rem`, borderStyle: `solid`, borderWidth: `5px`, borderRadius: `20%`}}>\ */}
       <Container style={{ maxWidth: `100px`, marginBottom: `1.45rem`}}>
-          <LambdaImage/>
+          <StyledLambdaImage/>
         </Container>
         <p>Developer // Brisbane // Australia</p>
         <Container style={{ maxWidth: `200px`, marginBottom: `1.45rem` }}>
